cd3694e9243d7aca3e7082f51a48dac4c1027361
BadrBoukeloua <Boukeloua.Badr@outlook.com>
2023-04-27 17:40:36 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,17 @@
-#include "lists.h"
-
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        count++;
-        h = h->next;
-    }
-    return count;
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		num_nodes++;
+		h = h->next;
+	}
+
+	return num_nodes;
 }
