c3362073a29631bee1816beeb60f92c2f4ddf76f
BadrBoukeloua <125593677+BadrBoukeloua@users.noreply.github.com>
2023-07-06 23:25:57 +0100

    Update 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+
+* print_binary - converts a n number to binary and prints it
+
+* @n: number to convert and print in binary
+*/
+void print_binary(unsigned long int n)
+{
+    int index;
+
+    unsigned long int mask = sizeof(n) * 8;
+
+    while (!(n & mask) && mask)
+        mask >>= 1;
+
+    while (mask)
+    {
+        putchar((n & mask) ? '1' : '0');
+        mask >>= 1;
+    }
+
+    if (!n)
+        putchar('0');
+}
