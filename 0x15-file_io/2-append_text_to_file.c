2bb7c6dca821074ddc561673663576d9b2d4c48a
BadrBoukeloua <125593677+BadrBoukeloua@users.noreply.github.com>
2023-07-11 02:26:33 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,26 +15,26 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, wstatus;
+    FILE *file;
+    int result = -1;
 
-if (filename == NULL) return -1;
+ if (filename == NULL) return -1;
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
+    file = fopen(filename, "a");
+    if (file == NULL)
         return -1;
 
-    if (text_content == NULL)
+    if (text_content != NULL)
     {
-        close(fd);
-        return 1;
+        size_t len = strlen(text_content);
+        result = fwrite(text_content, sizeof(char), len, file);
+        if (result < len)
+        {
+            fclose(file);
+            return -1;
+        }
     }
 
-    size_t nbytes = strlen(text_content);
-    wstatus = write(fd, text_content, nbytes);
-    close(fd);
-
-    if (wstatus == -1)
-        return -1;
-
+    fclose(file);
     return 1;
 }
